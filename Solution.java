24424d4cc84c8e6860a009b8d5d8a564aa1a6691
ashu23111997 <35415652+ashu23111997@users.noreply.github.com>
2018-10-15 23:40:32 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,4 +1,13 @@
-jsanfasjkfnaskj
-asfjsanfksjaf
-asfjasnkfj
-asjkfasnfjasnfjasnfjas
+import java.util.*;
+class Solution{
+  public static void main(String[] args)
+  {
+    int num = (new Scanner(System.in)).nextInt();
+    if(num>0)
+      System.out.println("Positive");
+    else if(num<0)
+      System.out.println("Negative");
+    else
+      System.out.println("Zero");
+  }
+}
