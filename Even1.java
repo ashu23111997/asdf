4d983cb5072bd47c7515cdb4ff0f994b9fee6646
ashu23111997 <35415652+ashu23111997@users.noreply.github.com>
2018-10-15 23:47:53 +0530

    Create Even1.java

---
+++ Even1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Even1{
+  public static void main(String[] args)
+  {
+    int num = (new Scanner(System.in)).nextInt();
+    if(num<0)
+      System.out.println("Invalid");
+    else if(num%2==0)
+      System.out.println("Even");
+    else
+      System.out.println("Odd");
+  }
+}
