4471073da6515927b216e8a171c59df3d310462c
ashu23111997 <35415652+ashu23111997@users.noreply.github.com>
2018-10-18 09:00:56 +0530

    Update Cons.java

--- Cons.java
+++ Cons.java
@@ -4,14 +4,20 @@ class Cons{
 
 	public static void main(String[] args)
 		{
-			char a = (new Scanner(System.in)).nextLine().charAt(0);
-			if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
-			{
-				System.out.println("Vowel");
-			}
-			else if(((int)a>=65 && (int)a<=90) || ((int)a>=97 && (int)a<=122))
-				System.out.println("Consonant");
-			else
+			String x = (new Scanner(System.in)).nextLine();
+			char a = x.charAt(0);
+			if(x.length()>1)
 				System.out.println("invalid");
+			else
+			{
+				if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
+				{
+					System.out.println("Vowel");
+				}
+				else if(((int)a>=65 && (int)a<=90) || ((int)a>=97 && (int)a<=122))
+					System.out.println("Consonant");
+				else
+					System.out.println("invalid");
+			}
 		}
 }
